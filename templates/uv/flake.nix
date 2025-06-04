18d7fe6872060b4baf191ab98ad6ac223074884e
cswimr <seaswimmerthefsh@gmail.com>
2025-06-04 00:10:46 -0500

    add `ensurepip` invocation to the bootstrap script
    some python modules rely upon pip being installed. if the module/package does things properly, they should declare it as a dependency. some do not (red-discordbot being a prime example), so we ensure pip is always in the environment instead by invoking ensurepip. it won't fail if pip is present, so no harm no foul.

--- templates/uv/flake.nix
+++ templates/uv/flake.nix
@@ -107,6 +107,7 @@
 
                   source .venv/bin/activate
                   export PATH="${pkgs.ruff}/bin:${pkgs.basedpyright}/bin:$PATH"
+                  python -m ensurepip > /dev/null
                 '';
                 deps = [ "ensure-git-repository" ];
               };
